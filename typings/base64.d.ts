7c6e152cdce297c3065b4f47a93f1415d3ebe62c
rhl <rasmus@omnitech.dk>
2017-01-23 11:08:45 +0100

    Added base typings

---
+++ typings/base64.d.ts
@@ -0,0 +1,13 @@
+﻿// Type definitions for jQuery Plugin - base64 codec
+// Project: https://github.com/yatt/jquery.base64/
+// Definitions by: Shinya Mochizuki <https://github.com/enrapt-mochizuki/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+interface JQueryBase64Static {
+    encode(data: string, isUTF8?: boolean): string;
+    decode(data: string, isUTF8?: boolean): string;
+}
+
+interface JQueryStatic {
+    base64: JQueryBase64Static;
+}
