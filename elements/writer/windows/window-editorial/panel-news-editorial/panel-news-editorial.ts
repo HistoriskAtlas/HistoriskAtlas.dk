54e9fea14a173b0845650d583d4d0b72952b2032
syntax42 <syntax42@gmail.com>
2019-06-17 13:06:09 +0200

    10 newest geos shown in editorial window

---
+++ elements/writer/windows/window-editorial/panel-news-editorial/panel-news-editorial.ts
@@ -0,0 +1,74 @@
+﻿@component("panel-news-editorial")
+class PanelNewsEditorial extends polymer.Base implements polymer.Element {
+
+    @property({ type: Array })
+    public geos: Array<any>;
+
+    @property({ type: String, value: '' })
+    public filter: string
+
+    @property({ type: Boolean })
+    public selected: boolean;
+
+    @property({ type: Number, value: 0 })
+    public kind: number;
+
+    @observe('selected')
+    selectedChanged() {
+        if (this.selected && !this.geos) {
+            this.geos = [];
+            this.fetchGeos();
+        }
+    }
+
+    public fetchGeos() {
+        Services.get('geo', {
+            'schema': JSON.stringify(
+                {
+                    geo: {
+                        fields: [
+                            'id',
+                            'title',
+                            'created',
+                            { tag_geos: [{ tag: ['id', 'plurname', 'category'] }] }
+                        ],
+                        filters: {
+                            online: true
+                        }
+                    }
+                }
+            ),
+            'count': '10',
+            'sort': '{created:desc}'
+        }, (result) => {
+            this.updateGeos(result.data);
+        })
+    }
+
+    public updateGeos(newList: Array<any> = null) {
+        this.set('geos', (newList ? newList : this.geos));
+        this.$.list.notifyResize();
+    }
+
+    formatDate(date: string): string {
+        return Common.formatDate(date);
+    }
+
+    itemTap(e: any) {
+        Common.geoClick(e.model.item.id);
+    }
+
+    numberWithSeparaters(n: number): string {
+        return Common.numberWithSeparaters(n);
+    }
+
+    formatInstitutions(tag_geos: Array<any>): string {
+        var institutions: Array<string> = [];
+        for (var tag_geo of tag_geos)
+            if (tag_geo.tag.category == 3)
+                institutions.push(tag_geo.tag.plurname);
+        return institutions.join(', ');
+    }
+}
+
+PanelNewsEditorial.register();
