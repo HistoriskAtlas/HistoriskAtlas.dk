22251d15c74a87d3cdad1b07970b3e156a103622
syntax42 <syntax42@gmail.com>
2021-08-30 16:04:36 +0200

    Do not show timewarp on deep link

--- classes/Map/MainMap.ts
+++ classes/Map/MainMap.ts
@@ -109,7 +109,7 @@
         //setTimeout(() => this.updateExtent(), 1000);
         //setTimeout(() => this.updateExtentTimeWarp(), 1100);
 
-        setTimeout(() => App.global.set('timeWarpActive', LocalStorage.showTimeWarp || App.global.theme.secondarymapid), 0); //make sure the timewarp opens if it should
+        setTimeout(() => App.global.set('timeWarpActive', (LocalStorage.showTimeWarp && !App.passed.geo) || App.global.theme.secondarymapid), 0); //make sure the timewarp opens if it should
     }
 
     public updateExtent() {
