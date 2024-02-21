f1e30ac5927722d97eb50f41a4c12dc5fb3b6ec6
syntax42 <syntax42@gmail.com>
2024-02-21 10:42:06 +0100

    Make geos clickable on collection lists

--- elements/core/lists/collection-geo-list/collection-geo-list.ts
+++ elements/core/lists/collection-geo-list/collection-geo-list.ts
@@ -7,13 +7,13 @@ class CollectionGeoList extends polymer.Base implements polymer.Element {
     cgTap(e: any) {
         var collection_geo = <HaCollectionGeo>e.model.cg;
 
-        if (collection_geo.isViaPoint || collection_geo.geoIsPlaceholder) {
+        if (collection_geo.isViaPoint || (collection_geo.geoIsPlaceholder && (!collection_geo?.geo?.id))) {
             collection_geo.followExternalLink();            
             return;
         }
 
-        if (!App.haGeos.geos[collection_geo.geo.id])
-            return;
+        //if (!App.haGeos.geos[collection_geo.geo.id])
+        //    return;
 
         Common.dom.append(WindowGeo.create(collection_geo.geo));
         App.map.centerAnim(collection_geo.geo.coord, 1000, true);
