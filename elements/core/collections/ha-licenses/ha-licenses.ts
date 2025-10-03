c9fcdb852463484de25ed311b58a4ee88b16c867
Rasmus <rasmus@omnitech.dk>
2025-10-03 11:49:02 +0200

    Update script paths, licenses
    Updated the script source in `Default.aspx` to use the `bower_components` directory. Updated `ha-licenses.ts` to use newer Creative Commons license URLs and a new legal reference for Copyright.

--- elements/core/collections/ha-licenses/ha-licenses.ts
+++ elements/core/collections/ha-licenses/ha-licenses.ts
@@ -10,14 +10,14 @@ class HaLicenses extends polymer.Base implements polymer.Element {
         this.licenses = [];
         this.byTagID = [];
 
-        this.add(new HaLicens(80, 'Copyright', 'https://www.retsinformation.dk/Forms/R0710.aspx?id=164796'));
-        this.add(new HaLicens(271, 'Navngivelse-IkkeKommerciel-IngenBearbejdelse', 'http://creativecommons.org/licenses/by-nc-nd/2.5/dk/'));
-        this.add(new HaLicens(270, 'Navngivelse-IkkeKommerciel–DelPåSammeVilkår', 'http://creativecommons.org/licenses/by-nc-sa/2.5/dk/'));
-        this.add(new HaLicens(268, 'Navngivelse-IngenBearbejdelse', 'http://creativecommons.org/licenses/by-nd/2.5/dk/'));
-        this.add(new HaLicens(269, 'Navngivelse-IkkeKommerciel', 'http://creativecommons.org/licenses/by-nc/2.5/dk/'));
-        this.add(new HaLicens(267, 'Navngivelse-DelPåSammeVilkår', 'http://creativecommons.org/licenses/by-sa/2.5/dk/'));
-        this.add(new HaLicens(63, 'Navngivelse', 'http://creativecommons.org/licenses/by/2.5/dk/'));
-        this.add(new HaLicens(79, 'Public domain', 'https://creativecommons.org/about/pdm'));
+        this.add(new HaLicens(80, 'Copyright', 'https://www.retsinformation.dk/eli/lta/2014/1144'));
+        this.add(new HaLicens(271, 'Navngivelse-IkkeKommerciel-IngenBearbejdelse', 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.da'));
+        this.add(new HaLicens(270, 'Navngivelse-IkkeKommerciel–DelPåSammeVilkår', 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.da'));
+        this.add(new HaLicens(268, 'Navngivelse-IngenBearbejdelse', 'https://creativecommons.org/licenses/by-nd/4.0/deed.da'));
+        this.add(new HaLicens(269, 'Navngivelse-IkkeKommerciel', 'https://creativecommons.org/licenses/by-nc/4.0/deed.da'));
+        this.add(new HaLicens(267, 'Navngivelse-DelPåSammeVilkår', 'https://creativecommons.org/licenses/by-sa/4.0/deed.da'));
+        this.add(new HaLicens(63, 'Navngivelse', 'https://creativecommons.org/licenses/by/4.0/deed.da'));
+        this.add(new HaLicens(79, 'Public domain', 'https://creativecommons.org/public-domain/pdm/'));
     }
 
     private add(licens: HaLicens) {
